df8423fc6b4203040056c62cd6ff672d7a077cc5
Andrew VanNus <avannus@gmail.com>
2021-04-30 15:36:07 -0400

    Generations vs Starting Ratio added

--- main.py
+++ main.py
@@ -19,23 +19,39 @@ def neutral_moran(N, i=1, seed=0):
     return counts
 
 
+def sum_1(N, i):
+    sum1 = 0
+    for j in range(1, i + 1):
+        sum1 += float(N - i) / float(N - j)
+    return sum1
+
+
+def sum_2(N, i):
+    sum2 = 0
+    for j in range(i + 1, N):
+        sum2 += float(i) / float(j)
+    return sum2
+
+
 timeToRun = datetime.now()
 """
 Configure experimental variables
 """
 experimentPopulation = 20
-experimentCount = 9
-trialCount = 1000
+experimentCount = 19
+trialCount = 10000
+
+startAllelePop = []
 
 gens = []
-successes = []
+fixationsExperimentally = []
 for x in range(experimentCount):
     gens.append(0)
-    successes.append(0)
+    fixationsExperimentally.append(0)
+    startAllelePop.append(experimentPopulation / (experimentCount + 1) * (x + 1))
     for y in range(trialCount):
-        startAllelePop = (experimentPopulation / (experimentCount + 1)) * (x + 1)
         current_seed = y
-        allele_counts = neutral_moran(experimentPopulation, startAllelePop, current_seed)
+        allele_counts = neutral_moran(experimentPopulation, startAllelePop[x], current_seed)
         if y < 10:  # only save first 10 graphs
             plt.clf()  # clear figure
             plt.cla()  # clear axes
@@ -43,37 +59,59 @@ for x in range(experimentCount):
             plt.xlabel("Generations")
             plt.ylabel("Population")
             plt.title(
-                "Neutral Moran Population:{} Starting Allele Population:{}, Seed:{}".format(experimentPopulation,
-                                                                                            startAllelePop,
-                                                                                            current_seed))
+                "Moran Neutral Drift N = {}, i = {}, Seed = {}".format(experimentPopulation, startAllelePop[x],
+                                                                       current_seed))
             plt.savefig(
-                "graphs/Neutral Moran Pop:{} Start Allele Pop:{}, Seed:{}".format(experimentPopulation, startAllelePop,
-                                                                                  current_seed))
+                "graphs/Neutral Drift N = {} i = {}, Seed = {}".format(experimentPopulation, startAllelePop[x],
+                                                                       current_seed))
         gens[x] += len(allele_counts)
         if allele_counts[-1][0] != 0:
-            successes[x] += 1
+            fixationsExperimentally[x] += 1
+
+startingRatios = []
+for x in range(len(fixationsExperimentally)):
+    startingRatios.append(float(x + 1) / float(experimentCount + 1))
+
+#   define theoretical success rate per experiment
+fixationRatioMath = []
+for x in startingRatios:
+    fixationRatioMath.append(x)
 
 #   calc avg success rate per experiment
-avgSuccessRates = []
-for x in successes:
-    avgSuccessRates.append(float(x) / float(trialCount))
-startingRatios = []
-for x in range(len(successes)):
-    startingRatios.append(float(x + 1) / float(experimentCount + 1))
+fixationRatiosAvg = []
+for x in fixationsExperimentally:
+    fixationRatiosAvg.append(float(x) / float(trialCount))
 
 plt.clf()
 plt.cla()
-plt.plot(startingRatios, avgSuccessRates, 'ro')
-plt.plot([.1, .2, .3, .4, .5, .6, .7, .8, .9], [.1, .2, .3, .4, .5, .6, .7, .8, .9], 'b-')
+plt.plot(startingRatios, fixationRatioMath, 'b-')
+plt.plot(startingRatios, fixationRatiosAvg, 'ro')
 plt.axis([0, 1, 0, 1])
 plt.xlabel("Starting %")
 plt.ylabel("Fixation Rate")
 plt.title("Fixation Rate vs. Starting %")
 plt.savefig(
     "Fixation Rate Pop:{} Exp Count:{} Trial Count:{}".format(experimentPopulation, experimentCount, trialCount))
-#   calc avg gens per experiment
-avgGens = []
-for x in gens:
-    avgGens.append(x / trialCount)
 
-print "Completed in {} seconds.".format((datetime.now() - timeToRun).seconds)
+#   define theoretical gens per experiment
+generationsMath = []
+experimentRatios = []
+for x in range(1, experimentPopulation):
+    generationsMath.append(experimentPopulation * (sum_1(experimentPopulation, x) + sum_2(experimentPopulation, x)))
+    experimentRatios.append(float(x) / float(experimentPopulation))
+
+#   calc avg gens per experiment
+generationsAvg = []
+for x in gens:
+    generationsAvg.append(x / trialCount)
+plt.clf()
+plt.cla()
+plt.plot(experimentRatios, generationsMath, 'b-')
+plt.plot(startingRatios, generationsAvg, 'ro')
+plt.xlabel("Starting Allele Ratio")
+plt.ylabel("Generations")
+plt.title("Generations vs. Starting Allele Ratio")
+plt.savefig(
+    "Generation Count Pop:{} Exp Count:{} Trial Count:{}".format(experimentPopulation, experimentCount, trialCount))
+
+print "Completed at {} in {} seconds.".format(datetime.now(), (datetime.now() - timeToRun).seconds)
